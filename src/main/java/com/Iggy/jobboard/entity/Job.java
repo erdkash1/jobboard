a68f9230d87a87f4908d76459c923cf440d6e8dc
Erdkash1 <erdkash1@gmail.com>
2026-03-18 00:45:24 -0500

    add Job entity

---
+++ src/main/java/com/Iggy/jobboard/entity/Job.java
@@ -0,0 +1,89 @@
+package com.Iggy.jobboard.entity;
+
+import jakarta.persistence.*;
+
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "jobs")
+
+
+public class Job {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String company;
+    private String location;
+    private Double salary;
+    private String type;
+    private String description;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+}
